48039dfbda92e124b74291fe73c08cb7691f4fc2
Lucas Silva <lucas.s.l75@hotmail.com>
2020-01-25 15:39:49 -0300

    metodos subTotal e total

--- src/main/java/com/project/demoSpring/entities/OrderItem.java
+++ src/main/java/com/project/demoSpring/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public double getSubTotal() {
+		return quantity * price;
+	}
 
 	@Override
 	public int hashCode() {
